8edb928ce4db7b833b994950fcedd31a4ac3f7ce
Dennis de Klerk <dmdeklerk@gmail.com>
2022-06-20 11:24:27 +0200

    Add logging

--- utils.js
+++ utils.js
@@ -13,6 +13,7 @@ const dirSize = async directory => {
 const execCmd = command => {
   return new Promise((resolve,reject) => {
     exec(command, (err,output) => {
+      console.log('execCmd',{command,err,output})
       if (err) {
         reject(err)
       }
